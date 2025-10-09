ca4ca7c50b02fa298f5e6257d2c2fd46fe666d5c
Adriano <122603292+Adriano542@users.noreply.github.com>
2025-10-09 13:42:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,12 @@ const display = document.querySelector('#output');
 const numbers = [1, 3, 24, 5, 6, 13, 8, 19];
 const listElem = document.querySelector('ol');
 
-// 1. Mnożenie przez 2 (map)
 const doubled = numbers.map(function (elem) {
     return elem * 2;
 });
-display.innerHTML += `<p>Tablica *2: ${doubled}</p>`;
 
-// 2. Wyświetlenie oryginalnej tablicy
 display.innerHTML += `<p>Oryginalna tablica: ${numbers}</p>`;
-
+display.innerHTML += `<p>Tablica *2: ${doubled}</p>`;
 // 3. Filtr: tylko liczby nieparzyste
 const oddNumbers = numbers.filter(function (num) {
     return num % 2 !== 0;

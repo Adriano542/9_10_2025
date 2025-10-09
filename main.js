82c8499a3182f78bcfd72c5b1614549e7970387c
Adriano <122603292+Adriano542@users.noreply.github.com>
2025-10-09 14:40:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+let names = [
+  'Kowal',
+  'Lis',
+  'Nowak',
+  'Nowakowski',
+  'Iksiński',
+  'Milewski',
+  'Kamińska',
+  'Lasik',
+  'Burzyński',
+  'Nowankowski',
+  'Woźnicka'
+];
 
+let lista = document.querySelector("ul");
+let przycisk = document.querySelector("button");
+
+function losuj(n) {
+  return names.sort(() => Math.random() - 0.5).slice(0, n);
+}
+
+function filtrujKrotsze(tab) {
+  return tab.filter(e => e.length < 8);
+}
+
+function pokaz(tab) {
+  lista.innerHTML = tab.map(e => `<li>${e}</li>`).join("");
+}
+
+przycisk.onclick = () => {
+  let wylosowane = losuj(5);
+  let krotsze = filtrujKrotsze(wylosowane);
+  pokaz(krotsze);
+};
